5e6501b4454937ff8e549406e3ee0665f0575cec
Usman Hafiz <m.u.hafiz@outlook.com>
2026-04-01 05:07:57 +0100

    Update unlock type enum type

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,6 +1,6 @@
 import type { Database } from "./supabase";
 
 export type NotificationType = Database['public']['Enums']['notification_type'];
-export type AchievementType = Database['public']['Enums']['achievement_type'];
+export type UnlockType = Database['public']['Enums']['unlock_type'];
 export type PrivacyType = Database['public']['Enums']['privacy_type'];
 export type PrivacyNeededType = Database['public']['Enums']['privacy_needed_type'];
